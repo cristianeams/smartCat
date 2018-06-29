080196b34057e588b63fbd43a043c1cc470634b1
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-29 00:24:11 +0000

    Added jquery to update user profile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,5 +6,12 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+  $("#changes").on("click", function () {
+    var input = $("input#name").val();
+    var imgUrl = $("input#url").val();
+    $("h4").text(input);
+    $("#profile_pic").attr("src", imgUrl).modal('hide');
+  })
+
 });
