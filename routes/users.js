5a272fd6b25dea1bb6ce0153e1a80acc71574954
kaylavu <kaylaavu@gmail.com>
2018-06-29 21:45:58 +0000

    Added movie category

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,46 @@
 "use strict";
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const categoryGen = require('../lib/generateCategory')
+// const movie = require('../lib/movieAPI')
 module.exports = (knex) => {
   router.get("/:id", (req, res) => {
     knex
       .select("id")
-      .from("users").where('id','=', req.params.id)
+      .from("users").where('id', '=', req.params.id)
       .then((results) => {
         var userId = results[0].id
         //console.log('RESULT:',userId);
         return userId;
         //res.json(results);
-    }).then((userId) => {
-      knex.select("description" , 'category_id').from("tasks").where('users_id','=',userId).then((tasks)=> {
-        console.log(tasks)
-        res.json(tasks);
-      })
-    });
+      }).then((userId) => {
+        knex.select("description", 'category_id').from("tasks").where('users_id', '=', userId).then((tasks) => {
+          console.log(tasks)
+          res.json(tasks);
+        })
+      });
   });
 
 
-  router.post("/:id", (req,res) => {
-    var tasktext = req.body.text; 
-    var userId = req.params.id; 
-   var category = categoryGen.generateCategory(); 
-    console.log('tasktext', tasktext)
-    knex.insert({description: tasktext, users_id:userId, category_id:category}).into('tasks').then(function(id) {
-    console.log('userID', userId, "tasktext", tasktext)
-    })
-  
-  }); 
+  router.post("/:id", (req, res) => {
+    var tasktext = req.body.text;
+    var userId = req.params.id;
+    categoryGen.generateCategory(tasktext).then((category) => {
+      console.log('tasktext', tasktext)
+      return knex.insert({
+        description: tasktext,
+        users_id: userId,
+        category_id: category
+      }).into('tasks')
+    }).then(function (id) {
+      console.log('userID', userId, "tasktext", tasktext)
+    }).catch(()=> {
+      console.log("cannot categorize")
+    }).then(() => {
+      res.send("good job")
+    } )
+
+  });
 
   return router;
 }
